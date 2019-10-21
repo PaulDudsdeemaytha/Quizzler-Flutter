bbdc3df4309c6eb4892ca353a490d63632d4c4bc
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-10-21 13:47:13 -0700

    Displaying Questions once user clicks on an answer

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   //<Icon> Means that we are announcing that the List is containing only Icon widgets
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    )
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
-
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -49,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -63,19 +59,22 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
-              ),
-              onPressed: () {
+                onPressed: () {
+                  setState(() {
+                    questionNumber++;
+                  });
+                }
                 //The user picked true.
-              },
-            ),
+                ),
           ),
         ),
         Expanded(
@@ -91,6 +90,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  questionNumber++;
+                });
                 //The user picked false.
               },
             ),
